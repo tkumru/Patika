c8ffb6376fb2947a1cb0ad681ca08badefcb3ea6
Talha Nebi Kumru <92807327+tkumru@users.noreply.github.com>
2022-02-05 00:35:41 +0300

    Create Ödev_3.sql

---
+++ SQL/SQL_Temelleri/Ödev_3.sql
@@ -0,0 +1,11 @@
+select country from country
+where country like 'A%a';
+
+select country from country
+where length(country) > 5 and country like '%n';
+
+select title from film
+where title ilike '%t%t%t%t%';
+
+select * from film
+where title like 'C%' and length > 90 and rental_rate=2.99;
